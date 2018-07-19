5acfcdfc1477d9f36988f9875cc1e4fc47748817
Dillon <Dillon_m_gregg@yahoo.com>
2018-07-19 12:15:19 -0400

    Button Text and jQuery added
    Made the beginings of the main site functionality

--- scripts/buttonHandler.js
+++ scripts/buttonHandler.js
@@ -1,2 +1,16 @@
 // stop that dang carousel from moving on its own
 $('.carousel').carousel({interval: false})
+
+$('.carousel-item').on('click', 'button', function(event){
+  var gather =  $(event.target).attr("data-gather");
+    if (gather == 'wood') {
+        var Msg = $RS.wood.harvMsg;
+        $('#notify').prepend('<div>' + Msg + '</div>');
+    } else if (gather == 'brick') {
+        var Msg = $RS.brick.harvMsg;
+        $('#notify').prepend('<div>' + Msg + '</div>');
+    } else if (gather == 'stone') {
+        var Msg = $RS.stone.harvMsg;
+        $('#notify').prepend('<div>' + Msg + '</div>');
+    }
+});
---
+++ scripts/resources.js
@@ -0,0 +1,24 @@
+var Resources = {
+  stores: {
+    Wood : 100,
+    Brick : 50,
+    Stone : 0,
+    Iron : 0
+  },
+
+  wood: {
+    harvMsg: 'Chopped some lumber.'
+  },
+  brick: {
+    harvMsg: 'Baked some clay bricks.'
+  },
+  stone: {
+    harvMsg: 'Quarried some stone.'
+  },
+  iron: {
+    harvMsg: 'Mined some Iron.'
+  },
+
+};
+
+var $RS = Resources;
